f547e0a3e309011b20bf0a9780b4fae9fbb7fc6a
cubOlson <cub.olson@gmail.com>
2021-11-01 16:26:50 -0600

    Try again, gitignore no catch

---
+++ 1-Monday/practice-for-week-10-express-route-order/server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Server is alive");
+});
+
+app.get('/hello', (req, res) => {
+    res.send("Hello, my friend!");
+});
+
+app.get('/goodbye', (req, res) => {
+    res.send("Goodbye, my friend!");
+});
+
+app.get('/goodbye/forever', (req, res) => {
+    res.send("So long. Farewell. Have a great life!");
+});
+
+app.get('/goodbye/until/:time', (req, res) => {
+    res.send(`Have a nice day! See you ${req.params.time}.`);
+});
+
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
